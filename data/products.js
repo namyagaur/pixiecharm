f52a4236f36bdd3e8c8ff4b243b7038ece388c8e
namyagaur <namyaagaur@gmail.com>
2025-09-03 20:14:50 +0530

    Added New Products

--- data/products.js
+++ data/products.js
@@ -4,32 +4,94 @@ export const products = [{
     name: 'Ariel Inspired Pendant',
     price: 1099
 },{
+    id : 'arielring',
+    img : "images/Ariel-Ring.png",
+    name: 'Ariel Inspired Ring',
+    price: 799
+},{
+    id : 'arieltiara',
+    img : "images/Ariel-Tiara.png",
+    name: 'Ariel Inspired Tiara',
+    price: 4999
+},{
     id : 'belletik',
     img : "images/Belle.png",
     name: 'Belle Inspired Pendant',
     price: 1399
 },{
+    id : 'bellering',
+    img : "images/Belle-Ring.png",
+    name: 'Belle Inspired Ring',
+    price: 899
+},{
+    id : 'belletiara',
+    img : "images/Belle-Tiara.png",
+    name: 'Belle Inspired Tiara',
+    price: 6999
+},{
     id : 'moanatik',
     img : "images/Moana.png",
     name: 'Moana Inspired Pendant',
     price: 999
+},{
+    id : 'moanaring',
+    img : "images/Moana-Ring.png",
+    name: 'Moana Inspired Ring',
+    price: 678
+},{
+    id : 'moanatiara',
+    img : "images/Moana-Tiara.png",
+    name: 'Moana Inspired Tiara',
+    price: 5889
 },
 {
     id : 'Rupenzaltik',
     img : "images/Rupenzal.png",
     name: 'Rupenzal Inspired Pendant',
     price: 1399
+},
+{
+    id : 'Rupenzaltik',
+    img : "images/Rupenzal-Ring.png",
+    name: 'Rupenzal Inspired Ring',
+    price: 699
+},
+{
+    id : 'Rupenzaltik',
+    img : "images/Rupenzal-Tiara.png",
+    name: 'Rupenzal Inspired Tiara',
+    price: 6799
 },{
     id : 'elsatik',
     img : "images/Elsa.png",
     name: 'Elsa Inspired Pendant',
     price: 1499
+},{
+    id : 'elsatik',
+    img : "images/Elsa-Ring.png",
+    name: 'Elsa Inspired Ring',
+    price: 899
+},{
+    id : 'elsatik',
+    img : "images/Elsa-Tiara.png",
+    name: 'Elsa Inspired Tiara',
+    price: 5799
 },
 {
     id : 'jasminetik',
-    img : "images/Jasmine.png",
+    img : "images/jasmine.png",
     name: 'Jasmine Inspired Pendant',
     price: 1199
+},{
+    id : 'jasminetik',
+    img : "images/jasmine-Ring.png",
+    name: 'Jasmine Inspired Ring',
+    price: 699
+},{
+    id : 'jasminetik',
+    img : "images/jasmine-Tiara.png",
+    name: 'Jasmine Inspired Tiara',
+    price: 6799
 },
 {
     id : 'sleepingbeautytik',
@@ -38,8 +100,31 @@ export const products = [{
     price: 1399
 },
 {
+    id : 'sleepingbeautytik',
+    img : "images/SleepingBeauty-Ring.png",
+    name: 'Sleeping Beauty Inspired Ring',
+    price: 879
+},
+{
+    id : 'sleepingbeautytik',
+    img : "images/SleepingBeauty-Tiara.png",
+    name: 'Sleeping Beauty Inspired Tiara',
+    price: 7399
+},
+{
     id : 'cinderellatik',
     img : "images/Cinderella.png",
     name: 'Cinderella Inspired Pendant',
     price: 1299
-}];
+},{
+    id : 'cinderellatik',
+    img : "images/Cinderella-Ring.png",
+    name: 'Cinderella Inspired Ring',
+    price: 799
+},{
+    id : 'cinderellatik',
+    img : "images/Cinderella-Tiara.png",
+    name: 'Cinderella Inspired Tiara',
+    price: 6799
+},
+];
